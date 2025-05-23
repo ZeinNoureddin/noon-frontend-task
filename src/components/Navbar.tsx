51209d51b67e31c609a64a4b17bf094758146ff2
Zein Noureddin <zein30ne@aucegypt.edu>
2025-05-23 14:12:15 +0300

    removed searchbar from navbar when we are in home page

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,28 +1,26 @@
-// src/components/Navbar.tsx
 "use client";
 
 import Link from "next/link";
-import SearchBar from "./SearchBar"; // your existing SearchBar, will be smaller here
+import { usePathname } from "next/navigation";
+import SearchBar from "./SearchBar";
 import styles from "../styles/Navbar.module.scss";
 
 export default function Navbar() {
+  const pathname = usePathname();
+
   return (
     <nav className={styles.navbar}>
       <div className={styles.navContainer}>
-        {/* 1) Brand on the left */}
         <div className={styles.brand}>
-          <Link href="/" aria-label="Go to homepage">
-            The Film Fanatic
-          </Link>
+          <Link href="/">The Film Fanatic</Link>
         </div>
 
-        {/* 2) Search in the center */}
-        <div className={styles.center}>
-          {/* Pass a prop to make it smaller, or override via CSS */}
-          <SearchBar small />
-        </div>
+        {pathname !== "/" && (
+          <div className={styles.center}>
+            <SearchBar small />
+          </div>
+        )}
 
-        {/* 3) Links on the right */}
         <div className={styles.links}>
           <Link href="/">Home</Link>
           <Link href="/favourites">Favourites</Link>
