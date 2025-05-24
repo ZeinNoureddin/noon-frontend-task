20d3ea947008009238d8d3bb6501e6241e6a1d22
Zein Noureddin <zein30ne@aucegypt.edu>
2025-05-24 10:55:02 +0300

    Added films ribbon in home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,13 @@
 import homeStyles from "@/styles/Home.module.scss";
 import commonStyles from "@/styles/Common.module.scss";
 import SearchBar from "@/components/SearchBar";
+import PopularRibbon from "@/components/PopularRibbon";
 
 export default function Home() {
   const styles = { ...commonStyles, ...homeStyles };
 
   return (
-    <main className={styles.pageWrapper}>
+    <main className={styles.homeMainContainer}>
       <section
         aria-labelledby="home-heading"
         className={styles.homePageWrapper}
@@ -14,6 +15,7 @@ export default function Home() {
         <h1 className={styles.homeHeader}>Find your favourite films</h1>
         <SearchBar />
       </section>
+      <PopularRibbon />
     </main>
   );
 }
---
+++ src/components/PopularRibbon.tsx
@@ -0,0 +1,52 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import Image from "next/image";
+import styles from "../styles/PopularRibbon.module.scss";
+
+interface Movie {
+  id: number;
+  poster_path: string | null;
+  title: string;
+}
+
+export default function PopularRibbon() {
+  const [movies, setMovies] = useState<Movie[]>([]);
+
+  useEffect(() => {
+    async function load() {
+      try {
+        const res = await fetch(
+          `https://api.themoviedb.org/3/movie/popular?api_key=${process.env.NEXT_PUBLIC_TMDB_API_KEY}&language=en-US&page=1`
+        );
+        const json = await res.json();
+        setMovies(json.results || []);
+      } catch (e) {
+        console.error("Failed to fetch popular:", e);
+      }
+    }
+    load();
+  }, []);
+
+  const track = [...movies, ...movies];
+
+  return (
+    <div className={styles.ribbon}>
+      <div className={styles.track}>
+        {track.map((m, i) =>
+          m.poster_path ? (
+            <div key={`${m.id}-${i}`} className={styles.slide}>
+              <Image
+                src={`https://image.tmdb.org/t/p/w342${m.poster_path}`}
+                alt={m.title}
+                width={120}
+                height={180}
+                style={{ objectFit: "cover" }}
+              />
+            </div>
+          ) : null
+        )}
+      </div>
+    </div>
+  );
+}
